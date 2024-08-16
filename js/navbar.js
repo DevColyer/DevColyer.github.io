ac319a401b31c0b1e7e605f4f37ca03365bf3504
DevColyer <dcolyer@spartaglobal.com>
2024-08-16 15:01:11 +0100

    Style Changes and improved usability

--- js/navbar.js
+++ js/navbar.js
@@ -1,13 +1,9 @@
 // JavaScript for toggling the navbar
 document.addEventListener('DOMContentLoaded', function () {
     var toggler = document.querySelector('.navbar-toggler');
-    var navContainer = document.getElementById('navbarNav');
+    var navbar = document.querySelector('.navbar');
 
     toggler.addEventListener('click', function () {
-        if (navContainer.classList.contains('show')) {
-            navContainer.classList.remove('show');
-        } else {
-            navContainer.classList.add('show');
-        }
+        navbar.classList.toggle('open');
     });
 });
